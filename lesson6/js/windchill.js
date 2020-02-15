1cda0646d3226e1c00fe18aa9c073eb07c78c07b
leoSasuke <leocavachini@hotmail.com>
2020-02-15 17:49:35 -0300

    javascript

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,11 @@
-var valor = document.getElementById("temp").textContent;
-console.log(valor);
+const hot = parseFloat(document.getElementById('temp').textContent);
+const faster = parseFloat(document.getElementById('speed').textContent);
+
+let f = 35.74+(0.6215*hot)-(35.75*Math.pow(fater,0.16))+0.4275*hot*Math.pow(faster,0.16);
+let windchill = Math.round(f);
+if (hot<=50 && speed > 3){
+    document.getElementById('chill') = windchill;
+}else{
+    document.getElementById('chill') = "not Today"
+}
 
