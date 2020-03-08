d10d566572b685d94a24187c4968a1fe1c34e2da
leoSasuke <leocavachini@hotmail.com>
2020-03-08 19:59:41 -0300

    teste

--- JSON_start/js/scripts.js
+++ JSON_start/js/scripts.js
@@ -9,6 +9,16 @@ fetch(apiURL)
     console.log(weatherInfo);
     
     document.getElementById('place').innerHTML=weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML=weatherInfo.main.temp;
+    document.getElementById('windSpeed').innerHTML=weatherInfo.wind.speed;
+
+    const iconcode = weatherInfo.weather[0].icon;
+    console.log(iconcode);
+    const icon_path = "//openweathermap.org/img/w/"+ +".png";
+    console.log("icon_path");
+
+    document.getElementById('weather_icon').src = icon_path;
+
 
  }); //end of "then" fat arrow function
 
