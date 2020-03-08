c15830279161d847b92cce6f29df3a2d40b749c0
leoSasuke <leocavachini@hotmail.com>
2020-03-08 19:19:29 -0300

    json

---
+++ JSON_start/js/scripts.js
@@ -0,0 +1,16 @@
+//ADD the key and change units to imperial
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=2172797&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial"
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    
+
+ }); //end of "then" fat arrow function
+
+
+
