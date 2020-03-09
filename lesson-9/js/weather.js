e912b2c57a2f7176d4d24626b234067cb59560b8
leoSasuke <leocavachini@hotmail.com>
2020-03-08 22:35:57 -0300

    5

--- lesson-9/js/weather.js
+++ lesson-9/js/weather.js
@@ -8,7 +8,7 @@ fetch(apiURL)
     //Once it comes back, display it to the console.
     console.log(weatherInfo);
     
-    document.getElementById('sky').innerHTML=weatherInfo.weather.description;
+    document.getElementById('ceu').innerHTML=weatherInfo.weather.main;
     document.getElementById('chill').innerHTML=weatherInfo.name;
     document.getElementById('temp').innerHTML=weatherInfo.main.temp;
     document.getElementById('speed').innerHTML=weatherInfo.wind.speed;
