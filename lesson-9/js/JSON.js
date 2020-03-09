8449a2ca4942c24554540665d2bd08f83f1fe817
leoSasuke <leocavachini@hotmail.com>
2020-03-09 20:45:35 -0300

    log

--- lesson-9/js/JSON.js
+++ lesson-9/js/JSON.js
@@ -5,7 +5,7 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject) {
-        //console.table(jsonObject); // temporary checking for valid response and data parsing
+        console.table(jsonObject); // temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
         //console.log(towns);
 
