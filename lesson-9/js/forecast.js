5ae438e803c925c967f08945ae3f7fe620eb9aba
leoSasuke <leocavachini@hotmail.com>
2020-03-10 20:02:41 -0300

    test

--- lesson-9/js/forecast.js
+++ lesson-9/js/forecast.js
@@ -13,7 +13,7 @@ myweekday[6] ="Saturday";
 console.log(myweekday[2]);
 
 //ADD the key and change units to imperial
-const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5879400&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial";
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
