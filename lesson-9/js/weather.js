a959862bd7d6114850ce91abeb3fc57762870367
leoSasuke <leocavachini@hotmail.com>
2020-03-12 21:01:19 -0300

    json update

--- lesson-9/js/weather.js
+++ lesson-9/js/weather.js
@@ -1,18 +1,18 @@
 //ADD the key and change units to imperial
-const apiURL = "//api.openweathermap.org/data/2.5/weather?q=preston,us&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial"
+const apiURLweather = "//api.openweathermap.org/data/2.5/weather?q=preston,us&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial"
 
 //Go fetch it and then wait for a response.
-fetch(apiURL)
+fetch(apiURLweather)
   .then((response) => response.json())
-  .then((weatherInfo) => {
+  .then((weatherinfo) => {
     //Once it comes back, display it to the console.
     //console.log(weatherInfo);
     
-    document.getElementById('sky').innerHTML=weatherInfo.weather[0].main;
+    document.getElementById('sky').innerHTML=weatherinfo.weather[0].main;
     
-    document.getElementById('temp').innerHTML=weatherInfo.main.temp;
-    document.getElementById('speed').innerHTML=weatherInfo.wind.speed;
-    document.getElementById('hum').innerHTML=weatherInfo.main.humidity;
+    document.getElementById('temp').innerHTML=weatherinfo.main.temp;
+    document.getElementById('speed').innerHTML=weatherinfo.wind.speed;
+    document.getElementById('hum').innerHTML=weatherinfo.main.humidity;
 
     /*const iconcode = weatherInfo.weather[0].icon;
     console.log(iconcode);
