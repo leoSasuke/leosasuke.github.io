0d02d674e793e02ac3ebf08dfbf1aa9fd1ee9273
leoSasuke <leocavachini@hotmail.com>
2020-03-15 14:34:12 -0300

    10

--- lesson-11/js/fishhaven.js
+++ lesson-11/js/fishhaven.js
@@ -36,7 +36,7 @@ weekday[4] = 'Thursday';
 weekday[5] = 'Friday';
 weekday[6] = 'Saturday';
 
-const apiURL ='////api.openweathermap.org/data/2.5/forecast?q=fish haven,us&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial';
+const apiURL ='////api.openweathermap.org/data/2.5/forecast?q=fish haven&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial';
 
 fetch(apiURL)
 .then((response) => response.json())
