b8720aa2979fb388f22cdab9171b8d5bf402d7d8
leoSasuke <leocavachini@hotmail.com>
2020-03-15 14:26:52 -0300

    cood

--- lesson-11/js/fishhaven.js
+++ lesson-11/js/fishhaven.js
@@ -1,5 +1,5 @@
 //ADD the key and change units to imperial
-const apiURLweather = "//api.openweathermap.org/data/2.5/forecast?lat={42.0369}&lon={-111.3963}&&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial"
+//const apiURLweather = "//api.openweathermap.org/data/2.5/forecast?lat={42.0369}&lon={-111.3963}&&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial"
 
 //Go fetch it and then wait for a response.
 fetch(apiURLweather)
@@ -36,7 +36,7 @@ weekday[4] = 'Thursday';
 weekday[5] = 'Friday';
 weekday[6] = 'Saturday';
 
-const apiURL ='//api.openweathermap.org/data/2.5/forecast?q=Fish Haven&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial';
+const apiURL ='////api.openweathermap.org/data/2.5/forecast?lat={42.0369}&lon={-111.3963}&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial';
 
 fetch(apiURL)
 .then((response) => response.json())
