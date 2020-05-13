1cba5f5e7b230c9db1b3d092ee9fb5aa60afb845
leoSasuke <leocavachini@hotmail.com>
2020-05-13 07:00:51 -0300

    react-fail

---
+++ react-course/src/pages/Root.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    s
+  } from "react-router-dom";
+import PagesPromotionSearch from './Promotion/Search/Search';
+
+const Root = ()=>{
+      return (
+          <Router>
+              <Switch>
+                  <Route path='/' component={PagesPromotionSearch}/>
+              </Switch>
+          </Router>
+      );
+  };
+
+  export default Root;
