c1a6e10ad1f33b0bfd6d74acbb4e5ca154b20eaa
leoSasuke <leocavachini@hotmail.com>
2020-05-25 14:28:12 -0300

    sem redux

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,6 +1,9 @@
+//----import css---bootstrap---custom
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
+import '../template/custom.css'
 
+//----react
 import React from 'react'
 import Menu from '../template/menu'
 import Routes from './routes'
